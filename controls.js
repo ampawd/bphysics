f910e9b2f90bbc90cdddfc23a8cd42b7b410508f
Aram <nemanoe@gmail.com>
2015-11-24 22:37:06 +0400

    total updates

--- controls.js
+++ controls.js
@@ -1,32 +1,35 @@
-function createControls() {
+function controls(params, update) {
 	//	angles
-	$(function() {
-		$( "#angles_slider" ).slider({
-			range: true,
-			min: 0,
-			max: 180,
-			values: [ 0, 180 ],
-			slide: function( event, ui ) {
-				$( "#angles_amount" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
-			}
-		});
+	// $(function() {
+		// $( "#angles_slider" ).slider({
+			// range: true,
+			// min: 0,
+			// max: 180,
+			// values: [ 0, 180 ],
+			// slide: function( event, ui ) {
+				// $( "#angles_amount" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+			// }
+		// });
 		
-		$( "#angles_amount" ).val( $( "#angles_slider" ).slider( "values", 0 ) +
-			" - " + $( "#angles_slider" ).slider( "values", 1 ) );
-	});
+		// $( "#angles_amount" ).val( $( "#angles_slider" ).slider( "values", 0 ) +
+			// " - " + $( "#angles_slider" ).slider( "values", 1 ) );
+	// });
 	
+	var EPS = 0.01;
 	//	speed
 	$( "#speed_slider" ).slider({
 		range: "max",
-		min: 0,
-		max: 100,
-		step: 0.5,
-		value: 20,
+		min: 0.001,
+		max: 30,
+		step: 1,
+		value: 15,
 		slide: function( event, ui ) {
-			$( "#speed_amount" ).val( ui.value  );
+			params.speed = ui.value;			
+			$( "#speed_amount" ).val( ui.value.toFixed(0) );			
+			update();
 		}
 	});
-	$( "#speed_amount" ).val( $( "#speed_slider" ).slider( "value" ) );	
+	$( "#speed_amount" ).val( parseInt($( "#speed_slider" ).slider( "value" )) );	
 	
 	//	gravity
 	$( "#gravity_slider" ).slider({
@@ -36,7 +39,9 @@ function createControls() {
 		step: 0.01,
 		value: 0.52,
 		slide: function( event, ui ) {
-			$( "#gravity_amount" ).val( ui.value  );
+			$( "#gravity_amount" ).val( ui.value );			
+			params.gravity = ui.value;
+			update();
 		}
 	});
 	$( "#gravity_amount" ).val( $( "#gravity_slider" ).slider( "value" ) );	
@@ -44,12 +49,14 @@ function createControls() {
 	//	fforce
 	$( "#ff_slider" ).slider({
 		range: "max",
-		min: 0,
-		max: 1,
+		min: 0.001,
+		max: 0.1,
 		step: 0.0001,
 		value: 0.0085,
-		slide: function( event, ui ) {
-			$( "#ff_amount" ).val( ui.value  );
+		slide: function( event, ui ) {			
+			$( "#ff_amount" ).val( ui.value < 0.002 ? 0 : ui.value );
+			params.ff = ui.value;
+			update();
 		}
 	});
 	$( "#ff_amount" ).val( $( "#ff_slider" ).slider( "value" ) );	
@@ -57,34 +64,40 @@ function createControls() {
 	//	dampening
 	$( "#dampening_slider" ).slider({
 		range: "max",
-		min: 0,
+		min: 0.001,
 		max: 1.0,
 		step: 0.01,
 		value: 0.71,
 		slide: function( event, ui ) {
-			$( "#dampening_amount" ).val( ui.value  );
+			$( "#dampening_amount" ).val( ui.value < 0.002 ? 1 : 
+				(1.0 - ui.value).toFixed(4) < 0.019 ? 0 : (1.0 - ui.value).toFixed(4) );				
+			params.dampFactorX = ui.value;
+			params.dampFactorY = ui.value;
+			update();
 		}
 	});
 	$( "#dampening_amount" ).val( $( "#dampening_slider" ).slider( "value" ) );		
 	
 	//	numballs
-	$( "#numballs_slider" ).slider({
+	$( "#numparticles_slider" ).slider({
 		range: "max",
 		min: 1,
-		max: 100,
+		max: 15,
 		step: 1,
 		value: 7,
 		slide: function( event, ui ) {
-			$( "#numballs_amount" ).val( ui.value  );
+			params.numParticles = ui.value;
+			$( "#particles_amount" ).val( ui.value  );
+			update();
 		}
 	});
-	$( "#numballs_amount" ).val( $( "#numballs_slider" ).slider( "value" ) );	
+	$( "#particles_amount" ).val( $( "#numparticles_slider" ).slider( "value" ) );		
 }
 
 function controlsSliding() {
-	var paramms = $("#params");		
-	var val = -paramms.height() + $(".toggle").height();
-	$(paramms).css({"top": val});
+	var params = $("#params");		
+	var val = -params.height() + $(".toggle").height();
+	$(params).css({"top": val});
 	var toggled = false;
 	
 	$("#toggle-button").click(function() {
@@ -93,7 +106,7 @@ function controlsSliding() {
 		}	else {
 			$(this).html("Show parameters")
 		}
-		$(paramms).animate({
+		$(params).animate({
 			top: toggled ? val : -val/6
 		});
 		toggled = !toggled;
--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-(function() {
-	window.requestAnimFrame = (function(callback) {
-		return window.requestAnimationFrame 
-				|| window.webkitRequestAnimationFrame
-				|| window.mozRequestAnimationFrame 
-				|| window.oRequestAnimationFrame 
-				|| window.msRequestAnimationFrame
-				|| function(callback) {
-							window.setTimeout(callback, 1000 / 60)
-					 };
-	})();
-	
+(function() {	
 	//	globals
 	var canvas = $('#main-canvas'),
 			ctx = canvas[0].getContext('2d'),
@@ -18,43 +7,32 @@
 			canvasHeight = $(canvas).attr("height"),
 			
 			animParams = {animIntervalId: 0},			
-			run = $("#run"),	
+			run = $("#run"),			
 			
-			//	default parameters
-			_GRAVITY = .52,
-			_FF =	0.0085,				//	fricion force
+			particles = [], 			
+			params = {
+				gravity: 			.52,
+				ff:						.0085,			
+				dampFactorX:	.71,	
+				dampFactorY:	.71,
+				speed:				15,
+				numParticles:	7,
+				radius:				16,
+				angles: 			[],
+				init: 				function() {
+												for (var i = 0; i < this.numParticles; i++) {
+													this.angles.push( [randomArbitary(0, 180), randomArbitary(0, 180)] );
+												}
+											}
+			},
 			
-			//	dampening factors
-			_DAMPFACTORX = .71,	
-			_DAMPFACTORY = .71,
+			degToRad = Math.PI/180,
+			radToDeg = 180/Math.PI,
 			
-			balls = [], 
-			numBalls = 7,
-			RADIUS = 20;
+			modes = ["molecules", "balls"],
+			currentMode = modes[0];
 			
-			
-	function Ball(x, y, r, vx, vy, color) {
-		this.x = x;
-		this.y = y;
-		this.r = r;
-		this.dampFactorX = _DAMPFACTORX;
-		this.dampFactorY = _DAMPFACTORY;
-		this.color = color ? color : "blue";
-		
-		this.draw = function() {			
-			ctx.beginPath();
-			ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI, true);
-			ctx.fillStyle = this.color;
-			ctx.fill();
-			ctx.closePath();
-		};
-	}	
-	
-	function setBallNumber(ball, i) {
-		ctx.font = RADIUS+"px arelia";
-		ctx.fillStyle = '#fff';
-		ctx.fillText(i+1, ball.x-5, ball.y+5);
-	}
+			params.init();
 	
 	function randomArbitary(min, max) {
 		return Math.floor(Math.random() * (max - min + 1) + min);
@@ -69,6 +47,25 @@
 			return color;
 	}	
 	
+	function Particle(x, y, r, vx, vy, dmpX, dmpY, color) {
+		this.x = x;
+		this.y = y;
+		this.r = r;
+		this.vx = vx;
+		this.vy = vy;
+		this.dampFactorX = dmpX ? dmpX : 1;
+		this.dampFactorY = dmpY ? dmpY : 1;
+		this.color = color ? color : "blue";		
+		this.draw = function() {			
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI, true);
+			ctx.fillStyle = this.color;
+			ctx.fill();
+			ctx.closePath();
+		};
+	}	
+	
+	//	some math functions
 	function add(v1, v2) {
 		return {
 			x: v1.x + v2.x,
@@ -109,13 +106,13 @@
 		};
 	} 
 	
-	function handleCollision(balls, i, j) {
-		var dx = balls[i].x - balls[j].x,
-				dy = balls[i].y - balls[j].y,
+	function handleMoleculesCollision(i, j) {
+		var dx = particles[i].x - particles[j].x,
+				dy = particles[i].y - particles[j].y,
 				c = dx*dx + dy*dy,
-				minDistance = balls[i].r + balls[j].r,				
-				v1 = {x: balls[i].vx, y: balls[i].vy},
-				v2 = {x: balls[j].vx, y: balls[j].vy};		
+				minDistance = particles[i].r + particles[j].r,				
+				v1 = {x: particles[i].vx, y: particles[i].vy},
+				v2 = {x: particles[j].vx, y: particles[j].vy};			
 			
 			if (c <= minDistance*minDistance) {
 				var un = normalize({x: dx, y: dy}),
@@ -138,39 +135,109 @@
 				v1 = add(v1n, v1t);
 				v2 = add(v2n, v2t);
 				
-				balls[i].vx = v1.x;
-				balls[j].vx = v2.x;				
-				balls[i].vy = v1.y;
-				balls[j].vy = v2.y;
+				particles[i].vx = v1.x;
+				particles[j].vx = v2.x;				
+				particles[i].vy = v1.y;
+				particles[j].vy = v2.y;					
 				
-				var vxa = Math.abs(v1.x) + Math.abs(v2.x);
-						
-				var overlapx = minDistance - Math.abs(balls[i].x - balls[j].x);				
-				balls[i].x += balls[i].vx / vxa * overlapx;
-				balls[j].x += balls[j].vx / vxa * overlapx;
+				var overlap = minDistance - Math.abs(Math.sqrt(c)),
+						vxa = Math.abs(v1.x) + Math.abs(v2.x),
+						vya = Math.abs(v1.y) + Math.abs(v2.y);
+				
+				particles[i].x += particles[i].vx / vxa * overlap; 
+				particles[j].x += particles[j].vx / vxa * overlap; 
+				particles[i].y += particles[i].vy / vya * overlap; 
+				particles[j].y += particles[j].vy / vya * overlap;			
 			}
 	}
 	
-	function runPhysics(balls, angles) {
-		var ball;
+	function handleBallsCollision(i, j) {
+		var dx = particles[i].x - particles[j].x,
+				dy = particles[i].y - particles[j].y,
+				c = dx*dx + dy*dy,
+				minDistance = particles[i].r + particles[j].r,				
+				v1 = {x: particles[i].vx, y: particles[i].vy},
+				v2 = {x: particles[j].vx, y: particles[j].vy};			
+			
+			//	need another algorithm to resolve colision
+			if (c <= minDistance*minDistance) {
+				var un = normalize({x: dx, y: dy}),
+				ut = {x: -un.y, y: un.x},					
+				v1n = dotProduct(un, v1),		
+				v1t = dotProduct(ut, v1),
+				v2n = dotProduct(un, v2),
+				v2t = dotProduct(ut, v2),
+				temp;
+				
+				temp = v1n;
+				v1n = v2n;
+				v2n = temp;
+				
+				v1n = multScalar(un, v1n);
+				v2n = multScalar(un, v2n);
+				v1t = multScalar(ut, v1t);
+				v2t = multScalar(ut, v2t);
+				
+				v1 = add(v1n, v1t);
+				v2 = add(v2n, v2t);
+				
+				particles[i].vx = v1.x;
+				particles[j].vx = v2.x;				
+				particles[i].vy = v1.y;
+				particles[j].vy = v2.y;	 	
+			}
+	}
+	
+	function molecules() {
+		var molecule;
+		var timer = 25;
 		animParams.animIntervalId = setInterval(function() {
 			ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 			
-			balls.sort(function(a, b) {
-				return a.x < b.x;
-			})
-			
-			for (var i = 0; i < balls.length; i++) {
-				ball = balls[i];
-				for (var j = i+1; j < balls.length && balls[i].x + RADIUS > balls[j].x - RADIUS; j++) {
-					handleCollision(balls, i, j);
-				}
-							
-				ball.x += ball.vx;
-				ball.y += ball.vy;				
+			for (var i = 0; i < particles.length; i++) {
+				molecule = particles[i];				
+				molecule.x += molecule.vx;
+				molecule.y += molecule.vy;
 				
-				ball.vx -= ball.vx * _FF;													//	friction force on x-axis
-				ball.vy += _GRAVITY;															//	acceleration given by gravity
+				if (molecule.y + molecule.r > canvasHeight || molecule.y - molecule.r < 0) {
+					if (molecule.y + molecule.r > canvasHeight) {
+						molecule.y -= molecule.y + molecule.r - canvasHeight;	
+					}	else {
+						molecule.y += molecule.r - molecule.y;
+					}					
+					molecule.vy = -molecule.vy;	
+				}
+				
+				if (molecule.x + molecule.r > canvasWidth || molecule.x - molecule.r < 0) {
+					if (molecule.x + molecule.r > canvasWidth) {
+						molecule.x -= molecule.x + molecule.r - canvasWidth;	
+					} else {
+						molecule.x += molecule.r - molecule.x;
+					}
+					molecule.vx = -molecule.vx;
+				}				
+				
+				for (var j = i+1; j < particles.length; j++) {
+					handleMoleculesCollision(i, j);
+				}
+				
+				molecule.draw();
+			}
+		}, timer);		
+	}
+
+	function balls() {
+		var ball;
+		animParams.animIntervalId = setInterval(function() {
+			ctx.clearRect(0, 0, canvasWidth, canvasHeight);			
+			for (var i = 0; i < particles.length; i++) {
+				ball = particles[i];							
+				ball.x += ball.vx;
+				ball.y += ball.vy;
+				
+				//console.log(ball.x)	
+				ball.vx -= ball.vx * params.ff;		//	friction with the ground
+				ball.vy += params.gravity;															
 				
 				if (ball.y + ball.r > canvasHeight || ball.y - ball.r < 0) {
 					if (ball.y + ball.r > canvasHeight) {
@@ -178,15 +245,12 @@
 					}	else {
 						ball.y += ball.r - ball.y;
 					}
-					ball.vy = -ball.vy * ball.dampFactorY;					//	dampening effect on y-axis	
-					
-					if (Math.abs(ball.vy) <= 3.0) {									//	realistic dampening
-						ball.dampFactorY -= 0.05;					
-					}	
-
-					if (Math.abs(ball.vy) <= 0.3) {									//	avoid infinite bouncing
-						ball.vy = 0.0;
-						ball.dampFactorY = 0.0;
+					ball.vy = -ball.vy * ball.dampFactorY;					
+					if (ball.dampFactorY > 0.05)
+						ball.dampFactorY -= 0.01;
+					else {
+						if (params.gravity > 1)
+						params.gravity -= 0.5;
 					}	
 				}
 				
@@ -196,92 +260,126 @@
 					} else {
 						ball.x += ball.r - ball.x;
 					}
-					ball.vx = -ball.vx * ball.dampFactorX;
+					ball.vx = -ball.vx*ball.dampFactorX;
+					if (ball.dampFactorX > 0.05)
+						ball.dampFactorX -= 0.01;
+					else {
+						if (params.ff >= 0)
+							params.ff -= 0.001;
+					}
 				}				
 				
+				for (var j = i+1; j < particles.length; j++) {
+					handleBallsCollision(i, j);
+				}
 				ball.draw();
-				//setBallNumber(balls[i], i);	
 			}
+		
 		}, 25);		
-	}
+	}	
 	
-	
-	function initialDraw(balls, numBalls, RADIUS) {	
-		balls.length = 0;
-		for (var i = 0; i < numBalls; i++) {
-			balls.push( new Ball(-RADIUS*2 + ((i+1)*canvasWidth/numBalls), 
-									canvasHeight-RADIUS,
-									RADIUS, 
-									0, 
-									0,
-									"#0FABC1") );		//	"#0FABC1"
-			balls[i].draw();
-			//setBallNumber(balls[i], i);
+	function createParticles(callback) {
+		particles = [];
+		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+		for (var i = 0; i < params.numParticles; i++) {
+			particles.push(
+				new Particle(
+				-params.radius*2 + ((i+1)*canvasWidth/params.numParticles), 
+				canvasHeight-params.radius,
+				params.radius, 
+				Math.cos( params.angles[i][0]*degToRad )*params.speed, 
+				Math.sin( params.angles[i][1]*degToRad )*params.speed,
+				params.dampFactorX,
+				params.dampFactorY,
+				getRandomColor()) //	"#0FABC1"
+			);
+			
+			callback(particles[i]);
 		}
 	}
 	
 	var clicked = false;
 	function onRunClicked(e) {		
-		var speed			= parseInt($( "#speed_amount" ).val()),
-				alpha_min = parseInt($( "#angles_slider" ).slider( "values", 0 )),
-				alpha_max = parseInt($( "#angles_slider" ).slider( "values", 1 )),
-				newGravity = parseFloat($( "#gravity_amount" ).val()),
-				newFF = parseFloat($("#ff_amount").val()),
-				newDAMPFACTOR = parseFloat($( "#dampening_amount" ).val()),
-				numBalls = parseInt($( "#numballs_amount" ).val());
-				
-				initialDraw(balls, numBalls, RADIUS);
-				
-				_GRAVITY = newGravity == _GRAVITY ? _GRAVITY : newGravity;	
-				_FF = newFF == _FF ? _FF : newFF;	
-				
-				_DAMPFACTORX = newDAMPFACTOR;
-				_DAMPFACTORY = newDAMPFACTOR;
-		
-		var angles = [];
-		for (var i = 0; i < balls.length; i++) {			
-			angles.push( [randomArbitary(alpha_min, alpha_max), randomArbitary(alpha_min, alpha_max)] );
-			balls[i].vx = Math.cos( angles[i][0]*Math.PI/180 )*speed;
-			balls[i].vy = Math.sin( angles[i][1]*Math.PI/180 )*speed;		
-			balls[i].dampFactorX = _DAMPFACTORX;	
-			balls[i].dampFactorY = _DAMPFACTORY;	
-			balls[i].draw();			
-		}		
+		var newSpeed				= parseInt($( "#speed_amount" ).val()),
+				newGravity 			= parseFloat($( "#gravity_amount" ).val()),
+				newFF 					= parseFloat($("#ff_amount").val()),
+				newDmpFactor		= parseFloat($( "#dampening_amount" ).val()),
+				newNumParticles = parseInt($( "#particles_amount" ).val());		
 		
 		if (!clicked) {
 			$(this).html("Stop");
-			runPhysics(balls, angles);	
-			
+			balls();				
 		} else {
-			$(this).html("Run");
+			$(this).html("Continue");
 			if (animParams.animIntervalId) {
-				ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-				clearInterval(animParams.animIntervalId);
-				var ball;
-				for (var i = 0; i < balls.length; i++) {
-					ball = balls[i];
-					ball.x = -RADIUS*2 + ((i+1)*canvasWidth/numBalls);
-					ball.y = canvasHeight-ball.r;
-					ball.vx = Math.cos(randomArbitary(alpha_min, alpha_max)*Math.PI/180)*speed;
-					ball.vy = Math.sin(randomArbitary(alpha_min, alpha_max)*Math.PI/180)*speed;						
-					ball.dampFactorX = _DAMPFACTORX;
-					ball.dampFactorY = _DAMPFACTORY;
-					balls[i].draw();
-					//setBallNumber(balls[i], i);
-				}
+				clearInterval(animParams.animIntervalId);				
 			}
 		}
+		
 		clicked = !clicked;			
 	};
 	
-	function setHandlers( ) {		
+	function setHandlers() {		
 		$(run).click(onRunClicked);	
+		$(window).resize(function() {			
+			init();	
+			createParticles(function(a) {				
+				a.draw();
+			});			
+		});
 	}	
 	
+	controls(params, function() {
+		if (params.numParticles > particles.length) {
+			var i = particles.length;
+			while (particles.length < params.numParticles) {
+				params.angles.push( [randomArbitary(0, 180), randomArbitary(0, 180)] );
+				particles.push(
+					new Particle(
+					randomArbitary(2*params.radius, canvasWidth), 
+					randomArbitary(2*params.radius, canvasHeight),
+					params.radius, 
+					Math.cos( params.angles[i][0]*degToRad ) * params.speed, 
+					Math.sin( params.angles[i++][1]*degToRad ) * params.speed,
+					params.dampFactorX,
+					params.dampFactorY,
+					getRandomColor()) //	"#0FABC1"
+				)
+			} 
+		} else if (params.numParticles < particles.length) {
+			while (particles.length > params.numParticles) {
+				particles.pop();
+				params.angles.pop();
+			}
+		}
+		 
+		for (var i = 0; i < params.numParticles; i++) {
+			particles[i].vx = (particles[i].vx/Math.abs(particles[i].vx))*params.speed;
+			particles[i].vy = (particles[i].vy/Math.abs(particles[i].vy))*params.speed;
+			
+			particles[i].dampFactorX = params.dampFactorX;
+			particles[i].dampFactorY = params.dampFactorY;
+		}	
+		
+		
+	});
 	
-	createControls();
+	function init() {
+		canvasWidth = canvas[0].parentNode.clientWidth;
+		canvasHeight = canvas[0].parentNode.clientHeight;
+		
+		canvas[0].width = canvasWidth;
+		canvas[0].height = canvasHeight;
+	}
+	
+	init();
 	controlsSliding();
 	setHandlers();
-	initialDraw(balls, numBalls, RADIUS);
 	
+	createParticles(function(a) {
+		//	on each particle init
+		a.draw();
+	});
+	
+
 }());

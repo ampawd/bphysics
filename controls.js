a3452c89bf738305fc1f1e10d5bdd85b0fc34f23
ampawd <arammisampawd@yahoo.com>
2015-11-24 12:00:43 +0400

    Update controls.js

--- controls.js
+++ controls.js
@@ -49,12 +49,12 @@ function controls(params, update) {
 	//	fforce
 	$( "#ff_slider" ).slider({
 		range: "max",
-		min: 0.001,
+		min: 0,
 		max: 0.1,
 		step: 0.0001,
 		value: 0.0085,
 		slide: function( event, ui ) {			
-			$( "#ff_amount" ).val( ui.value < 0.002 ? 0 : ui.value );
+			$( "#ff_amount" ).val( ui.value );
 			params.ff = ui.value;
 			update();
 		}
@@ -64,14 +64,14 @@ function controls(params, update) {
 	//	dampening
 	$( "#dampening_slider" ).slider({
 		range: "max",
-		min: 0.001,
+		min: 0,
 		max: 1,
 		step: 0.01,
 		value: 0.71,
 		slide: function( event, ui ) {
 			var a = ui.value < 0.002 ? 1 : 
 				(1.0 - ui.value).toFixed(4) < 0.019 ? 0 : (1.0 - ui.value).toFixed(4);
-			//var	b = ui.value < 0.002 ? 0 : ui.value;
+				//var b = ui.value < 0.002 ? 0 : ui.value;
 			$( "#dampening_amount" ).val( a );
 			params.dampFactorX = ui.value;
 			params.dampFactorY = ui.value;
